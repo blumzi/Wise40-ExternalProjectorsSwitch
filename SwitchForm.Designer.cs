120973bbf8103efc8cdd5c3a4dc674899496d776
Arie Blumenzweig <theblumz@gmail.com>
2025-07-30 11:31:35 +0300

    nicer labels

--- SwitchForm.Designer.cs
+++ SwitchForm.Designer.cs
@@ -37,14 +37,14 @@
             // 
             this.buttonOn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.buttonOn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.buttonOn.ForeColor = System.Drawing.Color.White;
             this.buttonOn.Location = new System.Drawing.Point(12, 12);
             this.buttonOn.Name = "buttonOn";
             this.buttonOn.Padding = new System.Windows.Forms.Padding(5);
             this.buttonOn.Size = new System.Drawing.Size(269, 170);
             this.buttonOn.TabIndex = 0;
-            this.buttonOn.Text = "I";
+            this.buttonOn.Text = "ON";
             this.buttonOn.UseVisualStyleBackColor = false;
             this.buttonOn.Click += new System.EventHandler(this.onClick);
             // 
@@ -53,14 +53,14 @@
             this.buttonOff.BackColor = System.Drawing.Color.Red;
             this.buttonOff.Enabled = false;
             this.buttonOff.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.buttonOff.ForeColor = System.Drawing.Color.White;
             this.buttonOff.Location = new System.Drawing.Point(12, 188);
             this.buttonOff.Name = "buttonOff";
             this.buttonOff.Padding = new System.Windows.Forms.Padding(5);
             this.buttonOff.Size = new System.Drawing.Size(269, 170);
             this.buttonOff.TabIndex = 1;
-            this.buttonOff.Text = "O";
+            this.buttonOff.Text = "OFF";
             this.buttonOff.UseVisualStyleBackColor = false;
             this.buttonOff.Click += new System.EventHandler(this.onClick);
             // 
@@ -68,7 +68,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 375);
+            this.ClientSize = new System.Drawing.Size(294, 370);
             this.Controls.Add(this.buttonOff);
             this.Controls.Add(this.buttonOn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
